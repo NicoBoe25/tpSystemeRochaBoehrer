6e8f893320b3229981ccc8bb9b62b82c18109807
Nicolas BOEHRER <nboehrer@laposte.net>
2021-02-07 18:27:16 +0100

    set list Distance find best solution

--- src/Enumeration.java
+++ src/Enumeration.java
@@ -2,19 +2,38 @@ import java.util.ArrayList;
 
 public class Enumeration {
     private ArrayList<Ville> listVilles;
+    private ArrayList<ArrayList<Ville>> listOfListVilles;
     private ArrayList<Double> listDistancesParcours;
 
     private int idMeilleureSolution;
     public int getIdMeilleureSolution() {return idMeilleureSolution; }
-
+    public void setIdMeilleureSolution(int idMeilleureSolution) {this.idMeilleureSolution = idMeilleureSolution;}
 
     public Enumeration(ArrayList<Ville> villeArrayList) {
         this.listVilles = villeArrayList;
+        listDistancesParcours = new ArrayList<>();
+
     }
 
-    public double getMeilleureDistance(){
-        double bestResult = 0.0;
-        return bestResult;
+    public void setListDistancesParcours(){
+        for (int i = 0; i < listOfListVilles.size(); i++) {
+            double distanceAParcourir =0.0;
+            ArrayList<Ville> tempArrayList = listOfListVilles.get(i);
+            for (int j = 0; j < listOfListVilles.get(i).size()-1; j++) {
+                distanceAParcourir += tempArrayList.get(j).calculDistance(tempArrayList.get(j+1));
+            }
+            listDistancesParcours.add(distanceAParcourir);
+        }
+    }
 
+
+    public void findMeilleureDistance(){
+        double bestResult = listDistancesParcours.get(0);
+        for (int i = 1; i < listDistancesParcours.size(); i++) {
+            if (bestResult<listDistancesParcours.get(i)){
+                bestResult = listDistancesParcours.get(i);
+                setIdMeilleureSolution(i);
+            }
+        }
     }
 }
