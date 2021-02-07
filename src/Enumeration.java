b6f6d886ac6b0871dd409ce148bf22680e882f3f
Nicolas BOEHRER <nboehrer@laposte.net>
2021-02-07 18:07:30 +0100

    begin enum

--- src/Enumeration.java
+++ src/Enumeration.java
@@ -1,10 +1,20 @@
 import java.util.ArrayList;
 
 public class Enumeration {
-    private ArrayList listVilles;
+    private ArrayList<Ville> listVilles;
+    private ArrayList<Double> listDistancesParcours;
+
+    private int idMeilleureSolution;
+    public int getIdMeilleureSolution() {return idMeilleureSolution; }
+
 
     public Enumeration(ArrayList<Ville> villeArrayList) {
         this.listVilles = villeArrayList;
+    }
+
+    public double getMeilleureDistance(){
+        double bestResult = 0.0;
+        return bestResult;
 
     }
 }
